f243ff8fbc76be3588409ba9c01596cacc831207
Rosendo Martinez <rosendomartinez270@gmail.com>
2023-07-12 15:11:17 -0500

    Implement scrolling feature
    The map scrolling game feature is implemented, however, still needs to be
    refactored and bugs that need to be fixed.

---
+++ Game_Code/Map/Map.py
@@ -0,0 +1,84 @@
+import math
+
+def getRelativePoint(p1, p2):
+    # returns p1 relative to p2
+    return {'x':-(p2['x'] - p1['x']), 'y':-(p2['y'] - p1['y'])}
+
+
+class Map:
+    def __init__(self,map_width,map_height,screen_width,screen_height):
+        self.map_width = map_width
+        self.map_height = map_height
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+    def getMapOn(self, x, y):
+        if (x == 0):
+            i = 1
+        elif (x > 0):
+            i = math.ceil(x / self.map_width)
+        else:
+            i = math.floor(x / self.map_width)
+        if (y == 0):
+            j = 1
+        elif (y > 0):
+            j = math.ceil(y / self.map_height)
+        else:
+            j = math.floor(y / self.map_height)
+        return [i, j]
+
+    def getImportantPointsOnScreen(self):
+        # points relitive to screen origin (top left)
+        # case 1: map is bigger than screen
+        points = [{'x':0,'y':0},{'x':self.screen_width,'y':0}, {'x':0,'y':self.screen_height},{'x':self.screen_width,'y':self.screen_height}]
+        p_x = self.map_width
+        p_y = 0
+        while (p_x <= self.screen_width and p_y <= self.screen_height):
+            if (p_x == 0):
+                if (p_y == 0 or p_y == self.screen_height):
+                    p_x += self.map_width
+                    continue
+            if (p_x == self.screen_width):
+                if (p_y == 0 or p_y == self.screen_height):
+                    p_x = 0
+                    p_y += self.map_height
+                    continue
+            points.append({'x':p_x,'y':p_y})
+            p_x += self.map_width
+            if (p_x > self.screen_width):
+                p_x = 0
+                p_y += self.map_height
+
+        return points
+
+    def getMapTopLeftPosRelToBg(self,indicies):
+        # case 1: -x -y
+        # case 2: -x y
+        # case 3: x -y
+        # case 4: x y
+        if (indicies[0] < 0):
+            if (indicies[1] < 0):
+                return {'x': indicies[0] * self.map_width,
+                        'y': indicies[1] * self.map_height}
+            else:
+                return {'x':indicies[0]*self.map_width,
+                        'y':indicies[1]*self.map_height-self.map_height}
+        else:
+            if (indicies[1] < 0):
+                return {'x': indicies[0] * self.map_width-self.map_width,
+                        'y': indicies[1] * self.map_height}
+            else:
+                return {'x': indicies[0] * self.map_width-self.map_width,
+                        'y': indicies[1] * self.map_height-self.map_height}
+
+if __name__ == '__main__':
+    print('getMapOn() tests')
+    myMap = Map(300,300,350,350)
+    print(myMap.getMapOn(2 * myMap.map_width, 3 * myMap.map_height) == [2,3])
+    print(myMap.getMapOn(1.5 * myMap.map_width, 1 * myMap.map_height) == [2,1])
+    print(myMap.getMapOn(-7.8 * myMap.map_width, 1 * myMap.map_height) == [-8,1])
+    print(myMap.getMapOn(-10.1 * myMap.map_width, -12.8 * myMap.map_height) == [-11,-13])
+    print('getRelativePoint test')
+    print(getRelativePoint({'x':0,'y':0},{'x':7,'y':10}) == {'x':-7,'y':-10})
+    print(getRelativePoint({'x': 1, 'y': 1}, {'x': 3, 'y': 2}) == {'x':-2,'y':-1})
+    print(getRelativePoint({'x': -7, 'y': 8}, {'x': 3, 'y': 2}) == {'x':-10,'y':6})
--- Game_Code/main.py
+++ Game_Code/main.py
@@ -1,8 +1,60 @@
 import pygame
+from Game_Code.Map.Map import *
+
+def getMapColor(m,n):
+    # M even,odd is black
+    # M even,even is white
+    # M odd,even is black
+    # M odd,odd white
+    sm = -1 if m < 0 else 1
+    sn = -1 if n < 0 else 1
+    m = math.fabs(m)
+    n = math.fabs(n)
+    if m % 2 != 0:
+        if n % 2 != 0:
+            if ((sm == 1 and sn == 1) or (sm == -1 and sn == -1)):
+                return 'black'
+            else:
+                return 'white'
+        else:
+            if ((sm == 1 and sn == 1) or (sm == -1 and sn == -1)):
+                return 'white'
+            else:
+                return 'black'
+    else:
+        if n % 2 != 0:
+            if ((sm == 1 and sn == 1) or (sm == -1 and sn == -1)):
+                return 'white'
+            else:
+                return 'black'
+        else:
+            if ((sm == 1 and sn == 1) or (sm == -1 and sn == -1)):
+                return 'black'
+            else:
+                return 'white'
+
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280,720))
+'''
+Breaks with this params:
+MAP_WIDTH = 30
+MAP_HEIGHT = 30
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+'''
+
+MAP_WIDTH = 30
+MAP_HEIGHT = 30
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+BackgroundMap = Map(MAP_WIDTH,MAP_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT)
+SPEED = 3
+
+MAPS = ['black','white']
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+screen_rect_rel_bg = screen.get_rect()
 
 clock = pygame.time.Clock()
 
@@ -12,7 +64,25 @@ while True:
             pygame.quit()
             raise SystemExit
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        screen_rect_rel_bg.left -= SPEED
+    if keys[pygame.K_d]:
+        screen_rect_rel_bg.left += SPEED
+    if keys[pygame.K_w]:
+        screen_rect_rel_bg.top -= SPEED
+    if keys[pygame.K_s]:
+        screen_rect_rel_bg.top += SPEED
+
+    #screen_rect_rel_bg.left += 200
     screen.fill("purple")
+    for point in BackgroundMap.getImportantPointsOnScreen():
+        map_indicies = BackgroundMap.getMapOn(screen_rect_rel_bg.left + point['x'],screen_rect_rel_bg.top + point['y'])
+        map_tl_pos_rel_bg = BackgroundMap.getMapTopLeftPosRelToBg(map_indicies)
+        map_tl_pos_rel_screen = getRelativePoint(map_tl_pos_rel_bg,{'x':screen_rect_rel_bg.left,'y':screen_rect_rel_bg.top})
+        map_rect = pygame.Rect(map_tl_pos_rel_screen['x'],map_tl_pos_rel_screen['y'],MAP_WIDTH,MAP_HEIGHT)
+        map_color = getMapColor(map_indicies[0],map_indicies[1])
+        pygame.draw.rect(screen,map_color,map_rect)
 
     pygame.display.flip()
     clock.tick(60)
