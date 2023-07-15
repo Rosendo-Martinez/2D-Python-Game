61da7ebc13341c3f5ff0ce9f420ee224d7a31572
Rosendo Martinez <rosendomartinez270@gmail.com>
2023-07-15 12:54:27 -0500

    Create SubMap Class

--- Game_Code/Map/Map.py
+++ Game_Code/Map/Map.py
@@ -5,12 +5,56 @@ class Screen:
         self._point = Point(x,y)
         self._width = width
         self._height = height
+        self._mappedSubMaps = None
     def moveBy(self,x,y):
         self._point.x += x
         self._point.y += y
     def moveTo(self,x,y):
         self._point.x = x
         self._point.y = y
+
+class SubMap:
+    def __init__(self, screen, mappedScreenPointRelativeToScreenOrigin, height, width):
+        self._screen = screen
+        self._mappedScreenPointRelativeToScreenOrigin = mappedScreenPointRelativeToScreenOrigin
+        self._height = height
+        self._width = width
+    def getSubMapData(self):
+        # returns a tuple (i,j,Point)
+        indices = self._getIndicies(self._getMappedScreenPointRelativeMapOrigin())
+        subMapTopLeftRelativeToScreenTopLeft = self._getSubMapTopLeftRelativeToScreenTopLeft(indices[0],indices[1])
+        return (indices,subMapTopLeftRelativeToScreenTopLeft)
+    def _getMappedScreenPointRelativeMapOrigin(self):
+        return self._screen._point + self._mappedScreenPointRelativeToScreenOrigin
+    def _getSubMapTopLeftRelativeToScreenTopLeft(self,i,j):
+        return self._getSubMapTopLeft(i,j).getRelativePoint(self._screen._point)
+    def _getSubMapTopLeft(self,i,j):
+        # returns the coordinates of the top left point on the given map relative to the map origin
+        if (i < 0):
+            if (j < 0):
+                return Point(i * self._width, j * self._height)
+            else:
+                return Point(i * self._width, j * self._height - self._height)
+        else:
+            if (j < 0):
+                return Point(i * self._width - self._width, j * self._height)
+            else:
+                return Point(i * self._width - self._width,
+                             j * self._height - self._height)
+    def _getIndicies(self, point):
+        if (point.x == 0):
+            i = 1
+        elif (point.x > 0):
+            i = math.ceil(point.x / self._width)
+        else:
+            i = math.floor(point.x / self._width)
+        if (point.y == 0):
+            j = 1
+        elif (point.y > 0):
+            j = math.ceil(point.y / self._height)
+        else:
+            j = math.floor(point.y / self._height)
+        return (i,j)
 
 class Point:
     def __init__(self,x,y):
@@ -22,6 +66,10 @@ class Point:
 
     def getRelativePoint(self,point_relative_to):
         return Point(-(point_relative_to.x - self.x), -(point_relative_to.y - self.y))
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    def __str__(self):
+        return f'Point(x={self.x},y={self.y})'
 
 class Map:
     def __init__(self,map_width,map_height,screen_width,screen_height):
@@ -93,3 +141,12 @@ if __name__ == '__main__':
     map_test_getPoints = Map(50, 50, 100, 100)
     print('getScreenPoints Tests')
     print(map_test_getPoints.getScreenPoints() == [Point(0, 0), Point(50, 0), Point(100, 0), Point(0, 50), Point(50, 50), Point(100, 50), Point(0, 100), Point(50, 100), Point(100, 100)])
+
+    #SubMap Tests
+    print('Screen & SubMap Tests')
+    myScreen = Screen(0,0,100,100)
+    mySubMap = SubMap(myScreen,Point(0,100),50,50)
+    myScreen.moveBy(100,0)
+    print(mySubMap.getSubMapData() == ((2,2),Point(-50,50)))
+    myScreen.moveTo(-200,-100)
+    print(mySubMap.getSubMapData() == ((-4, 1), Point(0,100)))
